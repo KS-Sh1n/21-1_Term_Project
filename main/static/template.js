f59ba1f7b6b0d8088d7ad9b6f9232a631b1a4c29
KS Shin <rundeep41@gmail.com>
2021-05-14 22:28:56 +0900

    210514

--- main/static/template.js
+++ main/static/template.js
@@ -5,15 +5,31 @@ function scrapeloading()
     document.getElementById('title').innerHTML = "Scrping Feeds";
 }
 
-function filter_feed()
+function sort(index)
 {
-    table = document.getElementById('sitefeed');
-    tr = table.getElementsByTagName("tr");
+    var table = $('th').parents('table').eq(0);
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer(index));
+    console.log(rows);
+    console.log();
+    this.asc = !this.asc
+    if (!this.asc){rows = rows.reverse()}
+    for (var i = 0; i < rows.length; i++){table.append(rows)}
 }
 
-function sort_feed(col)
+function comparer(index)
 {
-    var table = document.getElementById('sitefeed');
-    var tr = table.getElementsByTagName("tr");
-    
+    return function(a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+    }
 }
+
+function getCellValue(row, index)
+{
+    return $(row).children('td').eq(index).text()
+}
+
+$('#select').click(function(){
+    console.log("1321312321");
+    document.getElementById('checkbox').style.display='block';
+})
--- main/static/test.js
+++ main/static/test.js
@@ -1,14 +1,23 @@
-$('th').click(function(){
-    var table = $(this).parents('table').eq(0)
-    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+
+function sort(index)
+{
+    var table = $('th').parents('table').eq(0);
+    var rows = table.find('tr:gt(0)').toArray();
+    rows.sort(comparer(index));
     this.asc = !this.asc
     if (!this.asc){rows = rows.reverse()}
-    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
-})
+    for (var i = 0; i < rows.length; i++){table.append(rows)}
+}
 function comparer(index) {
     return function(a, b) {
-        var valA = getCellValue(a, index), valB = getCellValue(b, index)
-        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+        var row_a = $(a).children('td').toArr0-=ay();
+        var row_b = $(b).children('td').toArray();
+        console.log(row_a);
+        console.log(row_b);
+            return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+        }
     }
+function Zero_Matrix(row, index)
+{ 
+    return $(row).children('td').eq(index).text();
 }
-function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
